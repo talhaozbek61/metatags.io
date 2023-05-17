4a7ab3eabf83fdf1a70b285e49da024afe9e5240
Talha <talhaozbek04@gmail.com>
2023-05-17 19:10:06 +0300

    navbar is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-300 hover:underline">
-      Hello world!
-    </h1>
+    <Navbar />
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import logo from "../logo.svg"
+const Navbar = () => {
+    const [search, setSearch] = useState("https://metatags.io")
+    return ( 
+        <nav className="border-b min-[770px]:h-[92px] h-16 border-[#C7D3D9] flex items-center justify-between px-6">
+            <div className="flex items-center">
+                <img src={logo} alt="Logo" />
+                <a href="/" className="pl-4 text-lg font-bold -mt-[2px] hidden min-[641px]:inline">
+                Meta Tags
+                </a>
+            </div>
+            <div className="min-[770px]:w-[380px] h-12 w-[270px] rounded-md min-[770px]:-translate-x-[15px] max-[770px]:translate-x-1 z-10">
+                <input type="text" value={search} onChange={ e => setSearch(e.target.value) } id="search" className="bg-[#EBF1FA] w-full h-full min-[770px]:px-6 px-4  rounded-md font-bold text-base tracking-[0.02em] pt-[1px] focus:outline-none opacity-90" />
+            </div>
+            <div className="min-[770px]:inline-flex hidden">
+                <a href="/" className="mt-[2px] hover:text-[#2A81FB]">
+                Font Generator
+                </a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
