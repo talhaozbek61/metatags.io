5e8b7b25fa8046e794857a630798ffa07fa5bd0c
Talha <talhaozbek04@gmail.com>
2023-05-23 12:52:13 +0300

    rows problems are fixed

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -21,7 +21,7 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
             </div>
             <div className="mobile-shadow-google max-[769px]:px-3 max-[769px]:py-3 rounded-md">
             <div className="card-google">
-                <h1 className="text-[#1a0dab] min-[770px]:text-lg text-sm !leading-5 hover:underline cursor-pointer"> {title} </h1>
+                <h1 className="text-[#1a0dab] min-[770px]:text-lg text-sm !leading-5 hover:underline cursor-pointer break-words"> {title} </h1>
             </div>
             <div className="card-google-url text-[#006621] flex items-center">
                 <h1 className="text-sm mr-2"> {search}/</h1>
@@ -32,7 +32,7 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
                 </span>
             </div>
             <span className="w-full h-[1px] bg-slate-200 block mt-1 mb-1 min-[770px]:hidden"></span>
-            <p className="text-[#545454] text-[13px] google-description"> {description} </p>
+            <p className="text-[#545454] text-[13px] google-description break-words"> {description} </p>
             </div>
             </div>
             {/* Facebook */}
@@ -45,7 +45,7 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
                 <div className="card-facebook-image min-[770px]:h-[261px] min-[770px]:w-[500px] h-[180px] border border-gray-300"></div>
                 <div className="border-x border-b border-gray-300 px-3 py-[10px] bg-[#f2f3f5] text-[#606770] text-[12px] leading-3">
                     <span>{search.slice(8).toUpperCase()}</span>
-                    <p className="text-base mt-1 text-[#1d2129] font-semibold leading-[22px]"> {title} </p>
+                    <p className="text-base mt-1 text-[#1d2129] font-semibold leading-[22px] break-words"> {title} </p>
                     <p className="overflow-hidden text-sm whitespace-nowrap overflow-ellipsis leading-6"> {description} </p>
                 </div>
             </div>
@@ -61,8 +61,8 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
                 <div className="w-full">
                 <div className="card-twitter-image min-[770px]:h-[252px] min-[770px]:w-[504px] h-[150px] w-full border border-gray-200 rounded-t-lg group-hover:border-gray-300 duration-300"></div>
                 <div className="border-x border-b border-gray-200 px-[14px] py-2 text-[#606770] text-[12px] leading-3 rounded-b-lg group-hover:border-gray-300 duration-300">
-                    <p className="text-sm mt-1 text-[#1d2129] min-[770px]:font-bold leading-[1.3em]"> {title} </p>
-                    <p className="text-sm leading-[18px] mt-1 mb-1 text-black hidden min-[770px]:block"> {description} </p>
+                    <p className="text-sm mt-1 text-[#1d2129] min-[770px]:font-bold leading-[1.3em] break-words"> {title} </p>
+                    <p className="text-sm leading-[18px] mt-1 mb-1 text-black hidden min-[770px]:block break-words"> {description} </p>
                     <span className="text-sm text-[#8899A6]">{search.slice(8)}</span>
                 </div>
                 </div>
@@ -77,8 +77,8 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
             <div className="card-linkedin mt-[13px] min-[770px]:w-[552px] cursor-pointer">
                 <div className="card-linkedin-image min-[770px]:h-[288px] min-[770px]:w-[552px] h-[288px] border border-gray-300"></div>
                 <div className="border-x border-b border-gray-300 px-4 py-1 bg-[#f2f3f5] text-[#606770] text-[12px] leading-3">
-                    <p className="text-sm mt-1 text-[#1d2129] font-semibold leading-5"> {title} </p>
-                    <span className="leading-5">{search.slice(8)}</span>
+                    <p className="text-sm mt-1 text-[#1d2129] font-semibold leading-5 break-words"> {title} </p>
+                    <span className="leading-5 break-words">{search.slice(8)}</span>
                 </div>
             </div>
             </div>
@@ -91,7 +91,7 @@ const Preview = ({title, setTitle, description, setDescription, search}) => {
             <div className="card-pinterest mt-[13px] min-[770px]:w-[236px] cursor-zoom-in max-w-[236px]">
                 <div className="card-pinterest-image min-[770px]:h-[123px] min-[770px]:w-[236px] w-[236px] h-[123px] rounded-lg"></div>
                 <div className="px-2 text-[12px] leading-3 flex break-words">
-                    <p className="text-sm mt-1 text-[#1d2129] font-bold leading-[17px] -tracking-[0.4px]"> {title} </p>
+                    <p className="text-sm mt-1 text-[#1d2129] font-bold leading-[17px] -tracking-[0.4px] w-full"> {title} </p>
                     <span className="text-[#8e8e8e] mt-1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="fill-current w-[15px] h-[15px]">
                             <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/>
